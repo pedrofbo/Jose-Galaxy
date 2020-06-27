f4a52a28d850be0e99bd1ea004eb880e350f2635
Pedro <pedrofelipebo@gmail.com>
2020-06-27 03:08:20 -0300

    unity prototype in early stages of development

---
+++ Unity/Jose Galaxy/Assets/Scripts/PlanetClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetClass : MonoBehaviour {
+
+	public int planetClass;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		other.GetComponent<Jose>().planetClass = this.planetClass;
+	}
+}
---
+++ Unity/Jose Galaxy/Assets/Scripts/PlayerPlaceholder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class PlayerPlaceholder : MonoBehaviour
+{
+ 
+    public GameObject Player;
+    public GameObject Planet;
+
+	public int planetClass;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        //SMOOTH
+		
+        //POSITION
+        transform.position = Vector3.Lerp(transform.position, Player.transform.position, 0.1f);
+ 
+        Vector3 gravDirection;
+		if (planetClass == 1)
+		{
+			gravDirection = (transform.position - Planet.transform.position).normalized;
+			
+		}
+        else
+		{
+			gravDirection = (Planet.transform.up).normalized;
+		}
+ 
+        //ROTATION
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, gravDirection) * transform.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 0.1f);
+ 
+    }
+ 
+ 
+    public void NewPlanet(GameObject newPlanet) {
+ 
+        Planet = newPlanet;
+    }
+ 
+}
